cdb98303daf1ec5febf6b7c7341870fdf7031ff5
pkpraveen052 <40301280+pkpraveen052@users.noreply.github.com>
2026-03-26 21:03:52 +0530

    Fix dashboard assets template XMLID collision

--- metro_corporate_docs/models/dashboard.py
+++ metro_corporate_docs/models/dashboard.py
@@ -1,48 +1,10 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from odoo import api, models
 
 
-class MetroCorporateDocsDashboard(models.TransientModel):
+class MetroCorporateDocsDashboard(models.AbstractModel):
     _name = 'metro.corporate.docs.dashboard'
-    _description = 'Corporate Documents Dashboard'
-
-    address_change_count = fields.Integer(
-        string='Address Change Requests',
-        compute='_compute_record_counts',
-        store=False,
-    )
-    appointment_secretary_count = fields.Integer(
-        string='Appointment Secretary Requests',
-        compute='_compute_record_counts',
-        store=False,
-    )
-    resignation_secretary_count = fields.Integer(
-        string='Resignation Secretary Requests',
-        compute='_compute_record_counts',
-        store=False,
-    )
-
-    @api.depends()
-    def _compute_record_counts(self):
-        for rec in self:
-            rec.address_change_count = self.env['corporate.address.change'].search_count([])
-            rec.appointment_secretary_count = self.env['corporate.appointment.secretary'].search_count([])
-            rec.resignation_secretary_count = self.env['corporate.resignation.secretary'].search_count([])
-
-    def action_open_address_change(self):
-        action = self.env.ref('metro_corporate_docs.corporate_address_change_action').read()[0]
-        action.update({'target': 'current'})
-        return action
-
-    def action_open_appointment_secretary(self):
-        action = self.env.ref('metro_corporate_docs.corporate_appointment_secretary_action').read()[0]
-        action.update({'target': 'current'})
-        return action
-
-    def action_open_resignation_secretary(self):
-        action = self.env.ref('metro_corporate_docs.corporate_resignation_secretary_action').read()[0]
-        action.update({'target': 'current'})
-        return action
+    _description = 'Corporate Documents Dashboard Data Provider'
 
     @api.model
     def get_dashboard_data(self):
